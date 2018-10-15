285220d2812af9220abff56a14c9267396a8a5b3
MirzaHamzaManzoor <hamzamanzoor8234@gmail.com>
2018-10-15 17:09:22 +0500

    add page ok

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// time
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.timepicker');
+    var instances = M.Timepicker.init(elems);
+  });
+// date
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+  });
+  
